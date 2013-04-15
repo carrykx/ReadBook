a85f242390fe32f5b3971930f261f1ee8151c12d
carry <ibokan@bokanwensi3-28.local>
2013-04-15 14:31:27 +0800

    Initial Commit

---
+++ ReadBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReadBook
+//
+//  Created by carry on 13-4-15.
+//  Copyright (c) 2013年 kangxv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReadBook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ReadBook
+//
+//  Created by carry on 13-4-15.
+//  Copyright (c) 2013年 kangxv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
