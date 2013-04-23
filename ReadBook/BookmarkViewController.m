33ddd90cfc526c6db90deebb5ba0ae2714413c22
carry <ibokan@bokanwensi3-28.local>
2013-04-23 14:29:48 +0800

    修改细节

--- ReadBook/BookmarkViewController.m
+++ ReadBook/BookmarkViewController.m
@@ -6,10 +6,10 @@
 //  Copyright (c) 2013年 kangxv. All rights reserved.
 //
 
+
 #import "BookmarkViewController.h"
 #import "bookSave.h"
 #import "ReadBookViewController.h"
-
 @interface BookmarkViewController ()
 @property(nonatomic,retain)UITableView *tableView;
 @end
@@ -62,6 +62,81 @@
 #pragma mark tableView datasource delegate motheds
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
+    return [self.saveArray count];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString * cell  = @"cell";
+    UITableViewCell * _cell = [tableView dequeueReusableCellWithIdentifier:cell];
+    if (_cell == nil) {
+        _cell = [[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cell]autorelease];
+        _cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    //设置单元格的内容
+    bookSave * _bookSave = [[self.saveArray objectAtIndex:indexPath.row] retain];
+    NSLog(@"fffffff%@",_bookSave.name);
+    _cell.textLabel.font = [UIFont boldSystemFontOfSize:14];
+    _cell.textLabel.text = _bookSave.name;
+    _cell.imageView.image = [UIImage imageNamed:@"bookMark.png"];
+    return _cell;
+}
+//tableView 移动
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    bookSave * _bookSave = [[self.saveArray objectAtIndex:sourceIndexPath.row] retain];
+    [self.saveArray removeObjectAtIndex:sourceIndexPath.row];
+    [self.saveArray insertObject:_bookSave atIndex:destinationIndexPath.row];
     
+    [self _saveBookMark];
+}
+//tableView 可移动
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+//tableView 可编辑
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+//删除某一行
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        bookSave * _bookSave = [self.saveArray objectAtIndex:indexPath.row];
+        [self.saveArray removeObject:_bookSave];
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:[NSIndexPath indexPathForItem:indexPath.row inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
+    [self _saveBookMark];
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    ReadBookViewController * _readBook = [[ReadBookViewController alloc]init];
+    bookSave * _bookSave = [self.saveArray objectAtIndex:indexPath.row];
+    _readBook._readUrl = _bookSave.urlString;
+    _readBook.title = _bookSave.name;
+    [self.navigationController pushViewController:_readBook animated:YES];
+}
+#pragma mark
+#pragma mark public motheds
+//归档
+- (void)_saveBookMark
+{
+    NSString * homeStrng = NSHomeDirectory();
+    NSString * filePath = [homeStrng stringByAppendingFormat:@"webbook1.archiver"];
+    [NSKeyedArchiver archiveRootObject:self.saveArray toFile:filePath];
+}
+//tableView 可编辑
+- (void)_edit
+{
+    [self.tableView setEditing:YES animated:YES];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(_done)];
+    
+}
+//tableView 不可编辑
+- (void)_done
+{
+    [self.tableView setEditing:NO animated:YES];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemEdit target:self action:@selector(_edit)];
 }
 @end
